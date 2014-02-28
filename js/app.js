581645aa13689d6db8e251a4ec9b4da1bfe1dedd
Chris Allen <cehallen@gmail.com>
2014-02-28 14:00:52 -0500

    Finish simple version

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
-alert('Hey find me to know where to put the javascript');
+var max = 5;
+var number = Math.floor(Math.random() * max + 1);
+var name = prompt("What's your name?");
+
+debugger;
+var guess = prompt("Guess a number between 0 and " + max);
+if (guess === number) {
+  alert(name + ", you got it.")
+} else {
+  alert("Sorry " + name + ", you didn't get it.")
+}
